6d6e4eb4d81790467fdc5abe71d57ab484728230
Sarah <sarahlchavez@gmail.com>
2021-06-04 21:02:46 -0700

    drafted routes in wines controller

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,7 +1,31 @@
 class WinesController < ApplicationController
 
     get "/wines" do
-        erb :wines
+        @wines = Wine.all #see all of the wines
+        erb :index
     end
 
+    get "/wines/:id" do
+        #to see an invidual wine
+    end
+
+    get "/wines/new" do 
+        #form for creating a new wine
+    end
+
+    get "/wines/:id/edit" do 
+        #editing a specific wine
+    end
+
+    post "/wines" do
+        #create new wines
+    end
+
+    patch "/wines/:id" do
+        #editing a specific wine
+    end
+
+    delete "/wines/:id" do
+        #deleting an individual wine
+    end
 end
