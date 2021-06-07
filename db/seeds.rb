948b3bcc223c71a5692c18be18121f30e8f3befc
Sarah <sarahlchavez@gmail.com>
2021-06-07 11:12:54 -0700

    added data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,22 @@
 
 users = [
-    {username: '', email: '', password: ''}
+    {username: 'rsmith', email: 'rsmith@mail.com'}
+    {username: 'bjones', email: 'bjones@mail.com'}
 ]
 
+users.each do |u|
+    User.create(u)
+end
+
 wines = [
-    {producer_name: '', ava: '', vintage: '', varietal: ''}
-    {producer_name: '', ava: '', vintage: '', varietal: ''}
-    {producer_name: '', ava: '', vintage: '', varietal: ''}
+    {producer_name: 'Gilbert Cellars', ava: 'Wahluke Slope', vintage: '2015', varietal: 'Chardonnay'}
+    {producer_name: 'Red Car Wine Co.', ava: 'Fort Ross-Seaview', vintage: '2017', varietal: 'Pinot Noir'}
+    {producer_name: 'Merry Edwards', ava: 'Russian River Valley', vintage: '2019', varietal: 'Sauvignon Blanc'}
 ]
+
+wines.each do |w|
+    Wine.create(w)
+end
 
 # create_table "users", force: :cascade do |t|
 #     t.string "username"
