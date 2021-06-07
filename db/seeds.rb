0699d29d304364f127caa4e52a2ab5ff5e3b9ff6
Sarah <sarahlchavez@gmail.com>
2021-06-07 10:57:30 -0700

    created seed file

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+
+users = [
+    {username: '', email: '', password: ''}
+]
+
+wines = [
+    {producer_name: '', ava: '', vintage: '', varietal: ''}
+    {producer_name: '', ava: '', vintage: '', varietal: ''}
+    {producer_name: '', ava: '', vintage: '', varietal: ''}
+]
+
+# create_table "users", force: :cascade do |t|
+#     t.string "username"
+#     t.string "email"
+#     t.string "password"
+#   end
+
+
+# create_table "wines", force: :cascade do |t|
+#     t.string "producer_name"
+#     t.string "ava"
+#     t.integer "vintage"
+#     t.string "varietal"
+#     t.integer "user_id"
+#   end
