80e75aabd8c1897b1ff33db8cb7582c1c913f56e
Sarah <sarahlchavez@gmail.com>
2021-06-08 16:26:00 -0700

    updated users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     end
 
     post '/signup' do
-        u = User.new(params)
+        user = User.new(params)
         # We need to:
         # 1. instantiate a user
         # 2. ensure user signed up with valid data
@@ -13,23 +13,23 @@ class UsersController < ApplicationController
         # 4. if user didn't sign up properly, then redirect back to the form
     
         #if u.email.blank? || u.password.blank? || User.find_by_email(params["email"])
-        if !u.save
+        if !user.save
             redirect '/signup'
         else
-            u.save
+            user.save
             #binding.pry
-            session[:user_id] = u.id
+            session[:user_id] = user.id
             redirect '/wines' 
         end
     end
 
     get '/login' do
         # render the view in app/views/users/login.erb
-        erb :'/login'
+        erb :'users/login'
     end
     
     post '/login' do
-        @user = User.find_by(email: params[:email])
+        @user = User.find_by(email: params[:email]) # I don't think I need an instance variable here - *note:review that
         if @user
           session[:user_id] = @user.id
           redirect '/wines'
