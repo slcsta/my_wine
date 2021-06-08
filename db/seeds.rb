c87deb46d0758593b574395a00c63613d6d7053f
Sarah <sarahlchavez@gmail.com>
2021-06-07 21:26:32 -0700

    reseeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,10 @@
 
-# UPDATE MY SEED DATA to find_or_create_by to avoid duplication
+# use find_or_create_by to avoid duplication
 
-users = [
-    {username: 'rsmith', email: 'rsmith@mail.com'},
-    {username: 'bjones', email: 'bjones@mail.com'}
-]
+User.find_or_create_by(username: "rsmith", email: "rsmith@mail.com")
+User.find_or_create_by(username: "ajones", email: "ajones@mail.com")
+User.find_or_create_by(username: "etorres", email: "etorres@mail.com")
 
-users.each do |u|
-    User.create(u)
-end
-
-wines = [
-    {producer_name: 'Gilbert Cellars', ava: 'Wahluke Slope', vintage: '2015', varietal: 'Chardonnay'},
-    {producer_name: 'Red Car Wine Co.', ava: 'Fort Ross-Seaview', vintage: '2017', varietal: 'Pinot Noir'},
-    {producer_name: 'Merry Edwards', ava: 'Russian River Valley', vintage: '2019', varietal: 'Sauvignon Blanc'}
-]
-
-wines.each do |w|
-    Wine.create(w)
-end
-
-# create_table "users", force: :cascade do |t|
-#     t.string "username"
-#     t.string "email"
-#     t.string "password"
-#   end
-
-
-# create_table "wines", force: :cascade do |t|
-#     t.string "producer_name"
-#     t.string "ava"
-#     t.integer "vintage"
-#     t.string "varietal"
-#     t.integer "user_id"
-#   end
+Wine.find_or_create_by(producer_name: "Gilbert Cellars", wine_name: "Doc Stewart Chardonnay", ava: "Wahluke Slope", vintage: 2015, varietal: "Chardonnay")
+Wine.find_or_create_by(producer_name: "Red Car Wine Co.", wine_name: "Estate Pinot Noir", ava: "Fort Ross-Seaview", vintage: 2017, varietal: "Pinot Noir")
+Wine.find_or_create_by(producer_name: "Merry Edwards", wine_name: "Russian River Valley Sauvignon Blanc", ava: "Russian River Valley", vintage: 2019, varietal: "Sauvignon Blanc")
