d308f556635222ca32bbbc461f26d2c5e2079e11
Sarah <sarahlchavez@gmail.com>
2021-06-10 10:30:04 -0700

    added public folder and updated controllers and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,16 +45,6 @@ class UsersController < ApplicationController
 
     post '/logout' do
         session.clear 
-        redirect '/'
-    end
-
-    helpers do
-        def logged_in?
-            !!session[:user_id]
-        end
-
-        def current_user
-            User.find(session[:user_id])
-        end
+        redirect '/wines'
     end
 end
